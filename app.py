68a67fe6721e6b2dbd8f678f00047ba4cd12a797
zimbakill-beep <zimbakill@gmail.com>
2025-08-19 04:13:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -287,14 +287,14 @@ if st.session_state.step == 0:
 
             st.markdown("**Preencha sua data de nascimento** nos campos abaixo (Dia / Mês / Ano).")
             c1, c2, c3 = st.columns([1,1,2])
-            dia = c1.selectbox("Dia (nascimento)", list(range(1,32)), index=dia_default-1, placeholder="Selecione o dia")
-            mes = c2.selectbox("Mês (nascimento)", list(range(1,13)), index=mes_default-1, placeholder="Selecione o mês")
+            dia = c1.selectbox("Dia ", list(range(1,32)), index=dia_default-1, placeholder="Selecione o dia")
+            mes = c2.selectbox("Mês ", list(range(1,13)), index=mes_default-1, placeholder="Selecione o mês")
             anos = list(range(1950, hoje.year+1))
             try:
                 idx = anos.index(ano_default)
             except ValueError:
                 idx = len(anos)//2
-            ano = c3.selectbox("Ano (nascimento)", anos, index=idx, placeholder="Selecione o ano")
+            ano = c3.selectbox("Ano ", anos, index=idx, placeholder="Selecione o ano")
 
             identidade = st.selectbox("Como você se identifica? (opcional)", ["Feminino","Masculino","Prefiro não informar"],
                                       index=(["Feminino","Masculino","Prefiro não informar"].index(st.session_state.answers.get("identidade","Feminino")) if st.session_state.answers.get("identidade") else 0))
